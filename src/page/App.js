4aa416f1ac086a2cadf95521cf6d8b86b663a6a1
Ze Dang <4649tz@gmail.com>
2019-03-23 11:39:43 +0800

    table 使用ant design

--- src/page/App.js
+++ src/page/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-import { Button } from 'antd';
+import { Button, Table } from 'antd';
+import columns from './table-config';
+import rowData from './row-data';
 
 
 class App extends React.Component {
@@ -11,8 +13,8 @@ class App extends React.Component {
         this.state = {
             status: 0,
             person: 0,
-            columnDefs: null,
-            rowData: null,
+            columnDefs: columns,
+            rowData: rowData,
         }
     }
 
@@ -24,38 +26,38 @@ class App extends React.Component {
             const fs = window.require('fs');
             fs.readFile(filePaths[0], 'utf8', (err, contents) => {
                 if (err) { alert(err); }
-                contents = eval(contents);
-                this.setTableHeader(contents);
-                this.setTableRow(contents);
+                // contents = eval(contents);
+                // this.setTableHeader(contents);
+                // this.setTableRow(contents);
                 this.setState({status: 1});
             })
         });
     };
 
 
-    setTableHeader(contents) {
-        let headerArray = [];
-
-        for (let i = 0; i < contents.length; i++) {
-            headerArray = Array.from(new Set(headerArray.concat(Object.keys(contents[i]))));
-        }
-
-        let columnDefs = [];
-        for (let i = 0; i < headerArray.length; i++) {
-            let column = {};
-            column.headerName = headerArray[i];
-            column.field = headerArray[i];
-            column.sortable = true;
-            column.checkboxSelection = true;
-            columnDefs.push(column);
-        }
-
-        this.setState({columnDefs: columnDefs});
-    }
-
-    setTableRow(contents) {
-        this.setState({rowData: contents});
-    }
+    // setTableHeader(contents) {
+    //     let headerArray = [];
+    //
+    //     for (let i = 0; i < contents.length; i++) {
+    //         headerArray = Array.from(new Set(headerArray.concat(Object.keys(contents[i]))));
+    //     }
+    //
+    //     let columnDefs = [];
+    //     for (let i = 0; i < headerArray.length; i++) {
+    //         let column = {};
+    //         column.headerName = headerArray[i];
+    //         column.field = headerArray[i];
+    //         column.sortable = true;
+    //         column.checkboxSelection = true;
+    //         columnDefs.push(column);
+    //     }
+    //
+    //     this.setState({columnDefs: columnDefs});
+    // }
+    //
+    // setTableRow(contents) {
+    //     this.setState({rowData: contents});
+    // }
 
     render() {
         if (this.state.status === 0) {
@@ -64,14 +66,7 @@ class App extends React.Component {
             )
         } else {
             return (
-                <div className="ag-theme-balham table"
-                     style={{ height: '500px', width: '600px' }}>
-                    <button onClick={this.handleClickFocus}>Get selected rows</button>
-                    <AgGridReact columnDefs={this.state.columnDefs}
-                                 rowData={this.state.rowData}
-                                 onGridReady={ params => this.gridApi = params.api }>
-                    </AgGridReact>
-                </div>
+                <Table columns={this.state.columnDefs} dataSource={this.state.rowData} scroll={{ x: 1500, y: 300 }} size="small"/>
             )
         }
     }
