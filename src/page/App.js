c3b10d636008000f0de564f0002859ab3d9fb8ff
Ze Dang <4649tz@gmail.com>
2019-03-23 19:57:39 +0800

    实现表格展示

--- src/page/App.js
+++ src/page/App.js
@@ -1,75 +1,64 @@
-import React from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-import { Button, Table } from 'antd';
-import columns from './table-config';
-import rowData from './row-data';
+import React from 'react'
+import { Button, Table } from 'antd'
+import Focus from './Focus'
+import columnDefs from './table-config';
 
+/**
+ * TODO Implement features:
+ *      - 点击表格中行，高亮，传递数据
+ *      - Focus样式
+ *      - 可编辑样式
+ * */
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            status: 0,
-            person: 0,
-            columnDefs: columns,
-            rowData: rowData,
-        }
+  constructor (props) {
+    super(props)
+    this.state = {
+      status: 0,
+      person: 0,
+      columnDefs: null,
+      rowData: null,
     }
+  }
 
-    handleClickOn = () => {
-        const {dialog} = window.require('electron').remote;
-        dialog.showOpenDialog({properties: ['openFile']}, (filePaths) => {
-            if (filePaths == null) { return; }
+  handleClickOn = () => {
+    // 打开JSON文件
+    const { dialog } = window.require('electron').remote
+    dialog.showOpenDialog({ properties: ['openFile'] }, (filePaths) => {
+      if (filePaths == null) { return }
 
-            const fs = window.require('fs');
-            fs.readFile(filePaths[0], 'utf8', (err, contents) => {
-                if (err) { alert(err); }
-                // contents = eval(contents);
-                // this.setTableHeader(contents);
-                // this.setTableRow(contents);
-                this.setState({status: 1});
-            })
-        });
-    };
+      const fs = window.require('fs')
+      fs.readFile(filePaths[0], 'utf8', (err, contents) => {
+        if (err) { alert(err) }
+        contents = eval(contents);
+        this.setState({rowData: contents});
+        this.setState({status: 1});
+      })
+    })
+  }
 
-
-    // setTableHeader(contents) {
-    //     let headerArray = [];
-    //
-    //     for (let i = 0; i < contents.length; i++) {
-    //         headerArray = Array.from(new Set(headerArray.concat(Object.keys(contents[i]))));
-    //     }
-    //
-    //     let columnDefs = [];
-    //     for (let i = 0; i < headerArray.length; i++) {
-    //         let column = {};
-    //         column.headerName = headerArray[i];
-    //         column.field = headerArray[i];
-    //         column.sortable = true;
-    //         column.checkboxSelection = true;
-    //         columnDefs.push(column);
-    //     }
-    //
-    //     this.setState({columnDefs: columnDefs});
-    // }
-    //
-    // setTableRow(contents) {
-    //     this.setState({rowData: contents});
-    // }
-
-    render() {
-        if (this.state.status === 0) {
-            return (
-                <Button type="primary" onClick={this.handleClickOn}>Open</Button>
-            )
-        } else {
-            return (
-                <Table columns={this.state.columnDefs} dataSource={this.state.rowData} scroll={{ x: 1500, y: 300 }} size="small"/>
-            )
-        }
+  render () {
+    if (this.state.status === 0) {
+      return (
+        <Button type="primary" onClick={this.handleClickOn}>Open</Button>
+      )
+    } else {
+      return (
+        <div>
+          <Focus/>
+          <Table columns={columnDefs}
+                 dataSource={this.state.rowData}
+                 // scroll={{ x: 600, y: 300 }}
+                 size="small"
+                 onRow={(record) => ({
+                   onClick: () => {
+                     console.log(record.uuid)
+                   },
+                 })} />
+        </div>
+      )
     }
+  }
 }
 
-export default App;
+export default App
